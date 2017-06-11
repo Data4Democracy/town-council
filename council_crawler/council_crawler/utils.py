f66bc0f90b2bbe05311bc69ac8229db28749a25e
Andy <andy.s.bolton@gmail.com>
2017-06-11 13:27:23 -0600

    Added datetime util function

--- council_crawler/council_crawler/utils.py
+++ council_crawler/council_crawler/utils.py
@@ -1,7 +1,19 @@
 import hashlib
-
+from dateutil import parser
 
 def url_to_md5(url):
     m = hashlib.md5()
     m.update(url.encode())
     return m.hexdigest()
+
+def parse_date_string(date_string):
+    """find a date object in a string containing a date
+    """
+    date_string = date_string.replace("-", "/")
+    try:
+        date = parser.parse(date_string, fuzzy=True)
+    except ValueError:
+        return ""
+    day = "{0:02}".format(date.day)
+    month = "{0:02}".format(date.month)
+    return "{0}-{1}-{2}".format(date.year, month, day)
