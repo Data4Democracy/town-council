af68f6ee4b3bc895b4b84aa534e44f13b61a6085
Andy <andy.s.bolton@gmail.com>
2017-06-12 16:45:20 -0600

    Add datetime util function (#17)
    * Added datetime util function
    
    * Add Fremont spider
    
    * Added ca_fremont scrapy
    
    * Moved base_url reference to top
    
    * Added ca_fremont scrapy spider
    
    * Changed Dublin class name to Fremont
    
    * Accidentally removed backslash
    
    Accidentally removed backslash from Belmont scraper, adding it back
    
    * Modified parse_date_string() util
    
    Changed parse_date_string() util function to return a date object instead of a formatted string. Also added dateutil dependency to the requirements.txt file.
    
    * Added datetime util function
    
    * Modified parse_date_string() util
    
    Changed parse_date_string() util function to return a date object instead of a formatted string. Also added dateutil dependency to the requirements.txt file.
    
    * Fixed package name
    
    Fixed package name from 'dateutil' to 'python-dateutil'

--- council_crawler/council_crawler/utils.py
+++ council_crawler/council_crawler/utils.py
@@ -1,7 +1,17 @@
 import hashlib
-
+from dateutil import parser
 
 def url_to_md5(url):
     m = hashlib.md5()
     m.update(url.encode())
     return m.hexdigest()
+
+def parse_date_string(date_string):
+    """find a date object in a string containing a date
+    """
+    date_string = date_string.replace("-", "/")
+    try:
+        date = parser.parse(date_string, fuzzy=True)
+    except ValueError:
+        return None
+    return date
